d88b83e8144c07174b68e8a58fdbdc7adf410b51
Bence Bagics <bagics.bence.09@kodo.hu>
2021-06-24 17:34:33 +0200

    Initial Commit

---
+++ Bee/BeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeApp.swift
+//  Bee
+//
+//  Created by Macbook Pro CTO on 2021. 06. 24..
+//
+
+import SwiftUI
+
+@main
+struct BeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
