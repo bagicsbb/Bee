ae424ccf61a8618aef8cd0fa445415ee284b172c
Bence Bagics <bagics.bence.09@kodo.hu>
2021-06-27 20:43:55 +0200

    Sorting option added.

--- Bee/ProjectsView.swift
+++ Bee/ProjectsView.swift
@@ -11,6 +11,14 @@ struct ProjectsView: View {
     static let openTag: String? = "Open"
     static let closedTag: String? = "Closed"
     
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @State private var showingSortOrder = false
+    @State private var sortOrder = Item.SortOrder.optimized
+
+    
+    
     let showClosedProjects: Bool
     
     let projects: FetchRequest<Project>
@@ -26,25 +34,87 @@ struct ProjectsView: View {
             List {
                 ForEach(projects.wrappedValue) { project in
                     Section(header: ProjectHeaderView(project: project)) {
-                        ForEach(project.projectItems) { item in
+                        ForEach(items(for: project)) { item in
                             ItemRowView(item: item)
+                        }
+                        .onDelete { offsets in
+                            let allItems = project.projectItems
+                            
+                            for offset in offsets {
+                                let item = allItems[offset]
+                                dataController.delete(item)
+                            }
+                            dataController.save()
+                        }
+                        if showClosedProjects == false {
+                            Button {
+                                withAnimation {
+                                    let item = Item(context: managedObjectContext)
+                                    item.project = project
+                                    item.creationDate = Date()
+                                    dataController.save()
+                                }
+                            } label: {
+                                Label("Add New Item", systemImage: "plus")
+                            }
                         }
                     }
                 }
             }
             .listStyle(InsetGroupedListStyle())
             .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects" )
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if showClosedProjects == false {
+                        Button {
+                            withAnimation {
+                                let project = Project(context: managedObjectContext)
+                                project.closed = false
+                                project.creationDate = Date()
+                                dataController.save()
+                            }
+                        } label: {
+                            Label("Add Project", systemImage: "plus")
+                        }
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        showingSortOrder.toggle()
+                    } label: {
+                        Label("Sort", systemImage: "arrow.up.arrow.down")
+                    }
+                }
+            }
+            .actionSheet(isPresented: $showingSortOrder) {
+                ActionSheet(title: Text("Sort items"), message: nil, buttons: [
+                    .default(Text("Optimized")) { sortOrder = .optimized },
+                    .default(Text("Creation Date")) { sortOrder = .creationDate },
+                    .default(Text("Title")) { sortOrder = .title }
+                ])
+            }
         }
     }
-}
-
-struct ProjectsView_Previews: PreviewProvider {
     
-    static var dataController = DataController.preview
+    func items(for project: Project) -> [Item] {
+        switch sortOrder {
+        case .title:
+            return project.projectItems.sorted { $0.itemTitle < $1.itemTitle }
+        case .creationDate:
+            return project.projectItems.sorted { $0.itemCreationDate < $1.itemCreationDate }
+        case .optimized:
+            return project.projectItemsDefaultSorted
+        }
+    }
     
-    static var previews: some View {
-        ProjectsView(showClosedProjects: false)
-            .environment(\.managedObjectContext, dataController.container.viewContext)
-            .environmentObject(dataController)
+    struct ProjectsView_Previews: PreviewProvider {
+        
+        static var dataController = DataController.preview
+        
+        static var previews: some View {
+            ProjectsView(showClosedProjects: false)
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
     }
 }
---
+++ Bee/Sequence-Sorting.swift
@@ -0,0 +1,17 @@
+//
+//  Sequence-Sorting.swift
+//  Bee
+//
+//  Created by Macbook Pro CTO on 2021. 06. 27..
+//
+
+import Foundation
+
+extension Sequence {
+    func sorted<Value: Comparable>(by keyPath: KeyPath<Element, Value>) -> [Element] {
+        self.sorted {
+            $0[keyPath: keyPath] < $1[keyPath: keyPath]
+        }
+    }
+
+}
