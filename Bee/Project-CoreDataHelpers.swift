d1a3b0d7610fdb415aef0d4952ab564e25816f3c
Bence Bagics <bagics.bence.09@kodo.hu>
2021-06-29 20:08:11 +0200

    Core Data Predicates changed, item deleting bug fixed

--- Bee/Project-CoreDataHelpers.swift
+++ Bee/Project-CoreDataHelpers.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CoreData
+import SwiftUI
 
 extension Project {
     static let colors = ["Pink", "Purple", "Red", "Orange", "Gold", "Green", "Teal", "Light Blue", "Dark Blue", "Midnight", "Dark Gray", "Gray"]
@@ -55,6 +56,10 @@ extension Project {
         
         let completedItems = originalItems.filter(\.completed)
         return Double(completedItems.count) / Double(originalItems.count)
+    }
+    
+    var label: LocalizedStringKey {
+        LocalizedStringKey("\(projectTitle), \(projectItems.count) items, \(completionAmount * 100, specifier: "%g")% complete.")
     }
     
     static var example: Project {
